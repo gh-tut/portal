e9763a394065d95b9406316e881eb7611dc13e33
Shotaro Higashi <east9698@gmail.com>
2019-06-25 09:44:44 +0900

    Updates

--- precache-manifest.ea118a22f9662d169e562fe32f13c204.js
+++ precache-manifest.ea118a22f9662d169e562fe32f13c204.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e38d09ab227e9e19f6cb7ae4292e9498",
+    "revision": "ca7205bf6f1a6867f4e6d93457a6f09e",
     "url": "/portal/index.html"
   },
   {
-    "revision": "95e11a2cb95e86d6772f",
+    "revision": "7580ed0a0d56e4f835c3",
     "url": "/portal/static/css/main.9a893f94.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portal/static/js/2.67bac93f.chunk.js"
   },
   {
-    "revision": "95e11a2cb95e86d6772f",
-    "url": "/portal/static/js/main.e2d57f66.chunk.js"
+    "revision": "7580ed0a0d56e4f835c3",
+    "url": "/portal/static/js/main.4a8367e3.chunk.js"
   },
   {
     "revision": "05d4c05c31e6e52e5e67",
