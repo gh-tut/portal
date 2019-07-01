212251c0539dd940a864c3842e25ae132a102f46
Tepy Thai <thaitepy@gmail.com>
2019-07-01 15:17:54 +0900

    fix responsive calendar

--- src/components/IFrame.js
+++ src/components/IFrame.js
@@ -1,9 +1,32 @@
 import React from 'react';
-
-function IFrame({iframe}){
-  return <div>
-    <div dangerouslySetInnerHTML={{__html: iframe}}/>
-  </div>
+import styled from 'styled-components/macro';
+function IFrame({ iframe }) {
+  return (
+    <div
+      css={`
+        width: 100%;
+      `}
+    >
+      <div
+        css={`
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          iframe {
+            width: 90%;
+            max-width: 800px;
+          }
+          @media screen and (max-width: 479px) {
+            iframe {
+              width: 95%;
+            }
+          }
+        `}
+        dangerouslySetInnerHTML={{ __html: iframe }}
+      />
+    </div>
+  );
 }
 
 export default IFrame;
--- src/pages/Calender.js
+++ src/pages/Calender.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import IFrame from '../components/IFrame';
 import styled from 'styled-components/macro';
 import Selector from '../components/Selector';
+import Iframe from 'react-iframe';
 
 const iframe = `<iframe
         src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=MTg2OHV0bTQwbjFtNWd0YThwMm02YTdnMWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23616161"
@@ -38,28 +39,28 @@ const statusOption = [
 ];
 
 function renderCalendar(option) {
-  let iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=NHAxZGNndWwyNmJoaDhubDU4bzR0bGgxYThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23EF6C00" width="800" height="600" frameborder="0" scrolling="no"></iframe>`;
+  let iframe = `https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=NHAxZGNndWwyNmJoaDhubDU4bzR0bGgxYThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23EF6C00`;
   switch (option) {
     case `Meeting Room 1`:
-      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=NHAxZGNndWwyNmJoaDhubDU4bzR0bGgxYThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23EF6C00" width="800" height="600" frameborder="0" scrolling="no"></iframe>`;
+      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=NHAxZGNndWwyNmJoaDhubDU4bzR0bGgxYThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23EF6C00"  height="600" frameborder="0" scrolling="no"></iframe>`;
       break;
     case `Meeting Room 2`:
-      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=Y2dsbGNoMHU2cWY2MjI5bHIzYTNlbXYzdThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%239E69AF"  width="800" height="600" frameborder="0" scrolling="no"></iframe>`;
+      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=Y2dsbGNoMHU2cWY2MjI5bHIzYTNlbXYzdThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%239E69AF"  height="600" frameborder="0" scrolling="no"></iframe>`;
       break;
     case `Kitchen`:
-      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=MTg2OHV0bTQwbjFtNWd0YThwMm02YTdnMWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23616161"  width="800" height="600" frameborder="0" scrolling="no"></iframe>`;
+      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=MTg2OHV0bTQwbjFtNWd0YThwMm02YTdnMWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23616161"  height="600" frameborder="0" scrolling="no"></iframe>`;
       break;
     case `Monitor`:
-      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=MG5wdHE3dTFncGhvcTJjNDdpczR2cDU1dDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23F09300"  width="800" height="600" frameborder="0" scrolling="no"></iframe>`;
+      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=MG5wdHE3dTFncGhvcTJjNDdpczR2cDU1dDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23F09300"  height="600" frameborder="0" scrolling="no"></iframe>`;
       break;
     case `Whiteboard 1`:
-      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=aGczYms4czdzOW44dDF1Zmx0dmNldW45Z29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D50000"  width="800" height="600" frameborder="0" scrolling="no"></iframe>`;
+      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=aGczYms4czdzOW44dDF1Zmx0dmNldW45Z29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D50000"  height="600" frameborder="0" scrolling="no"></iframe>`;
       break;
     case `Whiteboard 2`:
-      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=ZXM1NDRtYTk2ZWx2OGs1ZjhwbTJmMnNndDBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23F09300"  width="800" height="600" frameborder="0" scrolling="no"></iframe>`;
+      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=ZXM1NDRtYTk2ZWx2OGs1ZjhwbTJmMnNndDBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23F09300"  height="600" frameborder="0" scrolling="no"></iframe>`;
       break;
     default:
-      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=NHAxZGNndWwyNmJoaDhubDU4bzR0bGgxYThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23EF6C00" width="800" height="600" frameborder="0" scrolling="no"></iframe>`;
+      iframe = `<iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=NHAxZGNndWwyNmJoaDhubDU4bzR0bGgxYThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23EF6C00"  height="600" frameborder="0" scrolling="no"></iframe>`;
       break;
   }
   return <IFrame iframe={iframe} />;
